c7f436d10a7a4caa990b962010eac1cafe1978c3
𝗖ʜᴏᴄᴏ <chocoXgithub@gmail.com>
2024-11-19 23:27:07 +0530

    Update start.py

--- AviaxMusic/utils/inline/start.py
+++ AviaxMusic/utils/inline/start.py
@@ -8,9 +8,12 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text= "ᴍᴜsɪᴄ" , callback_data="settings_back_helper"
+                text="ᴍᴜsɪᴄ", callback_data="settings_back_helper"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text="Management", callback_data="management_action"),  
+            [
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),# New "Management" button
+            ]
         ],
     ]
     return buttons
@@ -23,15 +26,15 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
         ],
-         
-        [ 
-           InlineKeyboardButton(
+        [
+            InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
-        )
+            )
         ],
-        [   
-           InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"), 
+        [
+            InlineKeyboardButton(text="ᴍᴜsɪᴄ", callback_data="settings_back_helper"),
+            InlineKeyboardButton(text="Management", callback_data="management_action"),  # New "Management" button
         ],
     ]
     return buttons
