ed022ab159a8a1be9d8ddff7dc3c2144a5090a90
𝗖ʜᴏᴄᴏ <chocoXgithub@gmail.com>
2024-11-19 09:48:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -101,6 +101,7 @@ SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/c7ea15528fc9816e05141-0a5598a
 # --------
 
 
+
 def time_to_seconds(time):
     stringt = str(time)
     return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
