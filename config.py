5c11cd5931b2e38fc2c32a0e53413d1c67c385bf
𝗖ʜᴏᴄᴏ <chocoXgithub@gmail.com>
2024-11-19 09:32:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,22 +7,19 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID" , "25475489"))
+API_HASH = getenv("API_HASH" , "3fc2b371f4fbb0166758736414d8be92")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN")
-
-# Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
-
-# Chat id of a group for logging bot's activities
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", None))
-
-# Get this value from @MissRose_Bot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", None))
+BOT_TOKEN = getenv("BOT_TOKEN" , "7617090520:AAH6OqlzkAGlJUhtd6O3yd9sRfDo6lxwn00")
+#-------------------------------------------------------
+OWNER_USERNAME = getenv("OWNER_USERNAME","UnknownX_9_11")
+#--------------------------------------------------------
+BOT_USERNAME = getenv("BOT_USERNAME" , "Fubuki_xbot")
+# --------------------------------------------------------
+BOT_NAME = getenv("BOT_NAME" , "˹𝙁ᴜʙᴜᴋɪ ✘ 𝐌ᴜsɪᴄ˼🫧")
+# ---------------------------------------------------------
+ASSUSERNAME = getenv("ASSUSERNAME" , "Not_ur_assistent")
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -32,15 +29,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/CyberPixelPro/AviaxMusic",
+    "https://github.com/Choco-criminal/AviaxMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NexGenBots")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/NexGenSpam")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Choco_for_u")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ANIME_CHAT_ANG")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -50,8 +47,10 @@ PRIVACY_LINK = getenv("PRIVACY_LINK", "https://telegra.ph/Privacy-Policy-for-Avi
 
 
 # Get this credentials from https://developer.spotify.com/dashboard
-SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
-SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
+SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "1c21247d714244ddbb09925dac565aed")
+SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "709e1a2969664491b58200860623ef19")
+# ----------------------------------------------------------------------------------
+
 
 
 # Maximum limit for fetching playlist's track from youtube, spotify, apple links.
@@ -65,7 +64,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496))
 
 
 # Get your pyrogram v2 session from Replit
-STRING1 = getenv("STRING_SESSION", None)
+STRING1 = getenv("STRING_SESSION1", "BQE4qWEAh0hMwsEORDJsTe-XW7wC7HMP8JKwKuOebOVxz6kQBMFfOgZPuQHD1RPX9yBtw5RWiw24Vp8qdO-FzXJvhP0DrwGLErxPlmmwxOzVLj5bJZ6XGD_QNOk-H0NyJZ8-pUb8TB85yC6HcKfXEztkVkghCo-Ol-_FiD-hVXsU2cN5g7GQs3_GdpnKC-MN6XZhdiEcgMxWYEXw4fbfG4Xzasvp6tE45rdkjWBfIBzPVzBL4tQqUtQzr9_uWIPrmIDjc4EmSy_xDefBi8jIhYkm84SokliLRpR8nbFxzXeUX2GCrrKoDWeT0aPhC0F3smP7Wy7Vxfn8nF1OwfbYnfrtcUhCPAAAAAHZxlQ5AA")
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
@@ -80,22 +79,26 @@ autoclean = []
 confirmer = {}
 
 
+------------------------------------------------------------------
+# ------------------------------------------------------------------------
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org//file/25115719697ed91ef5672.jpg"
+    "START_IMG_URL", "https://envs.sh/pC0.mp4"
 )
-PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org//file/389a372e8ae039320ca6c.png"
-)
-PLAYLIST_IMG_URL = "https://graph.org//file/3dfcffd0c218ead96b102.png"
-STATS_IMG_URL = "https://graph.org//file/99a8a9c13bb01f9ac7d98.png"
-TELEGRAM_AUDIO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
-TELEGRAM_VIDEO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+PING_IMG_URL = "https://envs.sh/AAt.mp4"
+PLAYLIST_IMG_URL = "https://envs.sh/p4F.jpg"
+STATS_IMG_URL = "https://envs.sh/pYG.mp4"
+TELEGRAM_AUDIO_URL = "https://envs.sh/pC3.jpg"
+TELEGRAM_VIDEO_URL = "https://envs.sh/pC3.jpg"
+STREAM_IMG_URL = "https://envs.sh/p4e.jpg"
+SOUNCLOUD_IMG_URL = "https://envs.sh/p4w.jpg"
+YOUTUBE_IMG_URL = "https://graph.org/file/4e4c15823f0056c0756c8-1230cfcb94e69c070c.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://graph.org/vTelegraphBot-10-21-13https://graph.org/vTelegraphBot-10-21-13"
+SPOTIFY_ALBUM_IMG_URL = "https://graph.org/vTelegraphBot-10-21-13https://graph.org/vTelegraphBot-10-21-13"
+SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/c7ea15528fc9816e05141-0a5598ac2585dae36f.jpg"
+
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+# --------
 
 
 def time_to_seconds(time):
